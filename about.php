8c5c23e69a699be95fdbfb5356c36e91c69ad971
Krisztian Kurucz <kkurucz@uwaterloo.ca>
2016-02-25 19:10:11 -0500

    Finishing initial upload

---
+++ about.php
@@ -0,0 +1,37 @@
+<?php include('header.php'); ?>
+   <!-- Page Header -->
+    <!-- Set your background image for this header on the line below. -->
+    <header class="intro-header" style="background-image: url('img/aboutme-bg.jpg')">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                    <div class="page-heading">
+                        <h1>About Me</h1>
+                        <hr class="small">
+                        <span class="subheading">This is who I am.</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <!-- Main Content -->
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                <p>Things to include:
+                <ul>
+                	<li>Pic of me (one from Gaspesie?)</li>
+                	<li>Short bio of origins - born, raised, lived, family, etc</li>
+                    <li>Languages I speak</li>
+                	<li>Paragraph of hobbies</li>
+                	<li>Blurb about skills / passions</li>
+                	<li>Ending thing saying hopeful to work with you, etc</li>
+                </ul>
+                </p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius praesentium recusandae illo eaque architecto error, repellendus iusto reprehenderit, doloribus, minus sunt. Numquam at quae voluptatum in officia voluptas voluptatibus, minus!</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum molestiae debitis nobis, quod sapiente qui voluptatum, placeat magni repudiandae accusantium fugit quas labore non rerum possimus, corrupti enim modi! Et.</p>
+            </div>
+        </div>
+    </div>
+<?php include('footer.php'); ?>
